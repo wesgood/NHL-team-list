63255505e212f14f697e408f31315281b8f8cc63
Wes Goodhoofd <wes@iversoft.ca>
2019-11-23 17:45:23 -0500

    project initialization and CocoaPod installation

---
+++ NHL teams/NHL teams/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  NHL teams
+//
+//  Created by Wes Goodhoofd on 2019-11-23.
+//  Copyright © 2019 Wes Goodhoofd. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ NHL teams/Pods/Target Support Files/Pods-NHL teams/Pods-NHL teams-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_NHL_teams : NSObject
+@end
+@implementation PodsDummy_Pods_NHL_teams
+@end
